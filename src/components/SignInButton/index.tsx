6753d36bffe52c7057237f4e5e0c7b5ad13c7745
gabriel-github <gabrielferrsantos201@gmail.com>
2021-07-27 01:26:21 -0300

    finished authentication

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,27 @@
+import styles from "./styles.module.scss";
+
+import { signIn, useSession } from "next-auth/client";
+
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
+export function SignInButton() {
+  const [session] = useSession();
+
+  return session ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Gabriel santos
+      <FiX className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="#eba417" />
+      Sign In with GitHub
+    </button>
+  );
+}
